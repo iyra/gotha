7b7886eff71e2b0a28efd0841598e116dea46c40
Iyra <iyra@geist.my.domain>
2019-01-21 12:03:02 +0000

    fixed the case of ((x+2) (y)) (two closing parens next to each other)

--- src/main.rs
+++ src/main.rs
@@ -21,6 +21,24 @@ struct Expr {
     cdr: Option<Box<Expr>>,
 }
 
+fn printexpr(m: Expr, nils: bool) {
+    match m.car {
+        Some(x) =>
+            match x.valtype {
+                Valtype::Whatval => print!("{}", match x.whatvalue {Some(v)=>v, None=>"???".to_string()}),
+                Valtype::Intval => match x.intvalue {Some(v)=>print!("{}",v), None=>print!("(int)???")},
+                Valtype::Exprval => { print!("(");
+                                      match x.exprvalue {Some(v)=>printexpr(*v, nils), None=>print!("(expr)???")}
+                                      print!(")") },
+            }
+        None => if nils { print!("[car nil]") }
+    }
+    match m.cdr {
+        Some(x) => {print!(" "); printexpr(*x, nils); },
+        None => if nils { print!("-> nil") }
+    }
+}
+
 fn parse(ins: String, m: Expr, in_expr: i32) -> (Expr, String) {
     println!("{:?}", m);
     match m.car {
@@ -28,68 +46,79 @@ fn parse(ins: String, m: Expr, in_expr: i32) -> (Expr, String) {
                 match ins.chars().next() {
                     Some(nc) => {
                         if !nc.is_whitespace() {
-                            match x.whatvalue {
-                                Some(v) => {
-                                    if nc == ';' {
-                                        for (i, c) in ins.chars().enumerate() {
-                                            if c == '\n' {
-                                                return parse(ins.chars().skip(i).take(ins.chars().count()).collect(), Expr {
-                                            car: Some(Value {
-                                                valtype: Valtype::Whatval,
-                                                whatvalue: Some(v),
-                                                intvalue: None,
-                                                exprvalue: None,
-                                            }),
-                                            cdr: m.cdr}, in_expr)
+                            match x.valtype {
+                                Valtype::Whatval => match x.whatvalue {
+                                    Some(v) => {
+                                        if nc == ';' {
+                                            for (i, c) in ins.chars().enumerate() {
+                                                if c == '\n' {
+                                                    return parse(ins.chars().skip(i).take(ins.chars().count()).collect(), Expr {
+                                                        car: Some(Value {
+                                                            valtype: Valtype::Whatval,
+                                                            whatvalue: Some(v),
+                                                            intvalue: None,
+                                                            exprvalue: None,
+                                                        }),
+                                                        cdr: m.cdr}, in_expr)
+                                                }
+                                            }
+                                            return (Expr {
+                                                car: Some(Value {
+                                                    valtype: Valtype::Whatval,
+                                                    whatvalue: Some(v),
+                                                    intvalue: None,
+                                                    exprvalue: None,
+                                                }),
+                                                cdr: m.cdr}, "".to_string())
+                                        }
+                                        if nc == ')' {
+                                            if in_expr > 0 {
+                                                return (Expr {
+                                                    car: Some(Value {
+                                                        valtype: Valtype::Whatval,
+                                                        whatvalue: Some(v),
+                                                        intvalue: None,
+                                                        exprvalue: None,
+                                                    }),
+                                                    cdr: m.cdr}, ins)
+                                            } else {
+                                                eprintln!("error: unexpected )");
+                                                process::exit;
                                             }
                                         }
-                                        return (Expr {
-                                            car: Some(Value {
-                                                valtype: Valtype::Whatval,
-                                                whatvalue: Some(v),
-                                                intvalue: None,
-                                                exprvalue: None,
-                                            }),
-                                            cdr: m.cdr}, "".to_string())
-                                    }
-                                    if nc == ')' {
-                                        if in_expr > 0 {
-                                        return (Expr {
-                                            car: Some(Value {
-                                                valtype: Valtype::Whatval,
-                                                whatvalue: Some(v),
-                                                intvalue: None,
-                                                exprvalue: None,
-                                            }),
-                                            cdr: m.cdr}, ins)
+                                        if nc != '(' {
+                                            /* stop people putting ( in the middle of a symbol... */
+                                            parse(ins.chars().skip(1).take(ins.chars().count()).collect(), Expr {
+                                                car: Some(Value {
+                                                    valtype: Valtype::Whatval,
+                                                    whatvalue: Some([v, nc.to_string()].join("")),
+                                                    intvalue: None,
+                                                    exprvalue: None,
+                                                }),
+                                                cdr: m.cdr}, in_expr)
                                         } else {
-                                            eprintln!("error: unexpected )");
-                                            process::exit;
+                                            eprintln!("error: unexpected (");
+                                            process::exit(1);
                                         }
-                                    }
-                                    if nc != '(' {
-                                        /* stop people putting ( in the middle of a symbol... */
-                                        parse(ins.chars().skip(1).take(ins.chars().count()).collect(), Expr {
-                                            car: Some(Value {
-                                                valtype: Valtype::Whatval,
-                                                whatvalue: Some([v, nc.to_string()].join("")),
-                                                intvalue: None,
-                                                exprvalue: None,
-                                            }),
-                                            cdr: m.cdr}, in_expr)
-                                    } else {
-                                        eprintln!("error: unexpected (");
+                                        
+                                    },
+                                    None => {
+                                        eprintln!("error: value type set to what but found nothing there");
                                         process::exit(1);
                                     }
-                                    
                                 },
-                                None => {
-                                    eprintln!("error: value type set to what but found nothing there");
+                                Valtype::Exprval => {                     
+                                        return (Expr {
+                                            car: Some(x),
+                                            cdr: m.cdr}, ins)
+                                },
+                                _ => {
+                                    eprintln!("error: unexpected value type in tree");
                                     process::exit(1);
                                 }
                             }
                         } else { // started collecting an int but now we have whitespace
-                            if in_expr > 0{
+                                if in_expr > 0{
                                     let (nextexpr, ni) = parse(ins.chars().skip(1).take(ins.chars().count()).collect(),
                                                          Expr{car:None, cdr:None}, in_expr+1);
                                     return (Expr {
@@ -115,7 +144,25 @@ fn parse(ins: String, m: Expr, in_expr: i32) -> (Expr, String) {
         None => { // car has not been filled yet
             match ins.chars().next() {
                 Some(nc) => {
-                    if nc == '(' {
+                    if nc == '\'' {
+                        let (nextexpr, ni) = parse(ins.chars().skip(1).take(ins.chars().count()).collect(),
+                                                   Expr{car:None, cdr:None}, in_expr+1);
+                        parse(ni.chars().skip(1).take(ins.chars().count()).collect(), Expr {
+                            car: Some(Value {
+                                valtype: Valtype::Exprval,
+                                whatvalue:None,
+                                intvalue: None,
+                                exprvalue: Some(Box::new(Expr{
+                                    car:Some(Value {
+                                        valtype: Valtype::Whatval,
+                                        whatvalue:Some("quote".to_string()),
+                                        intvalue: None,
+                                        exprvalue:None,
+                                    }),
+                                    cdr: Some(Box::new(nextexpr)),
+                                }))}),
+                            cdr: None}, in_expr)
+                    } else if nc == '(' {
                         let (nextexpr, ni) = parse(ins.chars().skip(1).take(ins.chars().count()).collect(),
                                                    Expr{car:None, cdr:None}, in_expr+1);
                         parse(ni.chars().skip(1).take(ins.chars().count()).collect(), Expr {
@@ -155,5 +202,27 @@ fn parse(ins: String, m: Expr, in_expr: i32) -> (Expr, String) {
 
 fn main() {
     println!("Hello, world!");
-    println!("{:?}", parse("a;hello\nb".to_string(), Expr{car:None, cdr:None}, 0));
+    //println!("{:?}", parse("'(a)".to_string(), Expr{car:None, cdr:None}, 0));
+    printexpr(parse("((lambda (x) (+ x 2)) '3)".to_string(), Expr{car:None, cdr:None}, 0).0, false);
 }
+
+
+/*(Expr {
+    car: Some(Value { valtype: Exprval, whatvalue: None, intvalue: None, exprvalue: Some(Expr {
+        car: Some(Value { valtype: Whatval, whatvalue: Some("quote"), intvalue: None, exprvalue: None}),
+        cdr: Some(Expr {
+            car: Some(Value { valtype: Whatval, whatvalue: Some("a"), intvalue: None, exprvalue: None }),
+            cdr: None }) }) }),
+    cdr: None }, "")*/
+
+/*(Expr {
+    car: Some(Value { valtype: Exprval, whatvalue: None, intvalue: None, exprvalue: Some(Expr {
+        car: Some(Value { valtype: Whatval, whatvalue: Some("quote"), intvalue: None, exprvalue: None}),
+        cdr: Some(Expr {
+            car: Some(Value { valtype: Exprval, whatvalue: None, intvalue: None, exprvalue: Some(Expr {
+                car: Some(Value { valtype: Whatval, whatvalue: Some("a"), intvalue: None, exprvalue: None }),
+                cdr: None }) }),
+            cdr: None }) }) }),
+    cdr: None }, "")
+
+(quote -> (a -> N) -> N) -> N*/
