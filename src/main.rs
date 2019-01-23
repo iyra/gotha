3885c0c788d352df77e20d42895c0e85d2a4ade8
Iyra <iyra@geist.my.domain>
2019-01-23 11:47:46 +0000

    added comment, string and escaping support to parser

--- src/main.rs
+++ src/main.rs
@@ -34,46 +34,60 @@ fn printexpr(m: Expr, nils: bool) {
         None => if nils { print!("[car nil]") }
     }
     match m.cdr {
-        Some(x) => {print!(" [cdr]-> "); printexpr(*x, nils); },
+        Some(x) => {print!(" "); printexpr(*x, nils); },
         None => if nils { print!("-> nil") }
     }
 }
 
-fn parse(ins: &mut dyn Iterator<Item=char>, m: Expr, in_expr: i32) -> Expr {
+fn parse(ins: &mut dyn Iterator<Item=char>, m: Expr, in_expr: i32, in_str: bool, escape: bool, in_comment: bool) -> Expr {
+    let escapable_chars = ['"', '\\', ';'];
     match ins.next() {
         Some(nc) => {
-            if nc.is_whitespace() {
-                if in_expr > 0 {
-                    match m.car {
-                        Some(mcar) => {
-                            let nextexpr = parse(ins, Expr { car: None, cdr: None }, in_expr);
-                            //println!("nextexpr: {:?} {:?}", nextexpr, Some(mcar));
-                            match nextexpr.car {
-                                Some(nec) => return Expr { car: Some(mcar), cdr: Some(Box::new(Expr{car:Some(nec), cdr:nextexpr.cdr})) },
-                                None => match nextexpr.cdr {
-                                    None => return Expr { car: Some(mcar), cdr: None },
-                                    Some(necdr) => return Expr { car: Some(mcar), cdr: Some(necdr) }
-                                },
-                            }
-                        },
-                        /* deal with the case of an opening paren followed by whitespace */
-                        None => return parse(ins, Expr { car: None, cdr: None }, in_expr)
+            println!("char = {}", nc);
+            if escape && !escapable_chars.contains(&nc) {
+                eprintln!("error: tried to escape a non-escapable character: {}", nc);
+                process::exit(1);
+            }
+
+            
+            if nc == ';' && !in_str {
+                return parse(ins, m, in_expr, in_str, escape, true);
+            } else if nc.is_whitespace() && !in_str {
+                if (in_comment && nc == '\n' && !escape) || !in_comment {
+                    if in_expr > 0 {
+                        match m.car {
+                            Some(mcar) => {
+                                let nextexpr = parse(ins, Expr { car: None, cdr: None }, in_expr, in_str, escape, false);
+                                //println!("nextexpr: {:?} {:?}", nextexpr, Some(mcar));
+                                match nextexpr.car {
+                                    Some(nec) => return Expr { car: Some(mcar), cdr: Some(Box::new(Expr{car:Some(nec), cdr:nextexpr.cdr})) },
+                                    None => match nextexpr.cdr {
+                                        None => return Expr { car: Some(mcar), cdr: None },
+                                        Some(necdr) => return Expr { car: Some(mcar), cdr: Some(necdr) }
+                                    },
+                                }
+                            },
+                            /* deal with the case of an opening paren followed by whitespace */
+                            None => return parse(ins, Expr { car: None, cdr: None }, in_expr, in_str, escape, false)
+                        }
+                    } else {
+                        return m;
                     }
                 } else {
-                    return m;
+                    return parse(ins, m, in_expr, in_str, escape, true)
                 }
             }
-            else if nc == '(' {
+            else if nc == '(' && !in_str && !in_comment {
                 match m.car {
                     None => {
-                        let nextexpr = parse(ins, Expr { car: None, cdr: None }, in_expr+1);
+                        let nextexpr = parse(ins, Expr { car: None, cdr: None }, in_expr+1, in_str, escape, false);
                         return parse(ins, Expr { car: Some(Value {
                             valtype: Valtype::Exprval,
                             whatvalue: None,
                             intvalue: None,
                             exprvalue: Some(Box::new(nextexpr))}),
-                        cdr: None,}, in_expr);
-                      
+                                                 cdr: None,}, in_expr, in_str, escape, false);
+                        
                     },
                     Some(ev) => {
                         eprintln!("error: unexpected ( in car");
@@ -81,7 +95,7 @@ fn parse(ins: &mut dyn Iterator<Item=char>, m: Expr, in_expr: i32) -> Expr {
                     },
                 }
             }
-            else if nc == ')' {
+            else if nc == ')' && !in_str && !in_comment {
                 if in_expr > 0 {
                     return m;
                 }
@@ -89,39 +103,50 @@ fn parse(ins: &mut dyn Iterator<Item=char>, m: Expr, in_expr: i32) -> Expr {
                     process::exit(1);
                 }
             }
-            else {
+            else if !in_comment {
                 match m.car {
                     None => return parse(ins, Expr {
                         car: Some(Value {
                             valtype: Valtype::Whatval,
-                            whatvalue: Some(nc.to_string()),
+                            whatvalue: if (nc == '"' && !escape) ||  (nc == '\\' && !escape) { Some("".to_string()) } else { Some(nc.to_string())  },
                             intvalue: None,
                             exprvalue: None, }),
-                        cdr: None,}, in_expr),
+                        cdr: None,}, in_expr, if nc == '"' && !escape { !in_str } else { in_str }, if nc == '\\' { !escape } else { false }, false),
                     Some(ev) => 
                         match ev.whatvalue {
                             Some(evw) => return parse(ins, Expr {
                                 car: Some(Value {
                                     valtype: Valtype::Whatval,
-                                    whatvalue: Some([evw, nc.to_string()].join("")),
+                                    whatvalue: if (nc == '"' && !escape) ||  (nc == '\\' && !escape) { Some(evw)  } else { Some([evw, nc.to_string()].join("")) },
                                     intvalue: None,
                                     exprvalue: None, }),
-                                cdr: None,}, in_expr),
+                                cdr: None,}, in_expr, if nc == '"' && !escape { !in_str } else { in_str }, if nc == '\\' { !escape } else { false }, false),
                             None => {
-                                eprintln!("no value found but we have something in car");
+                                eprintln!("{:?}", ev);
+                                eprintln!("error: car is overloaded with previous expression... are you missing a space?");
                                 process::exit(1);
                             }
                         }
                 }
             }
+            else {
+                return parse(ins, m, in_expr, in_str, escape, true);
+            }
         },
-        None => { return m; }
+        None => {
+            if in_expr == 0 {
+                return m;
+            } else {
+                eprintln!("error: unmatched parens by the end of input");
+                process::exit(1);
+            }
+        }
     }
 }
 
 
 fn main() {
     println!("Hello, world!");
-    printexpr(parse(&mut "( a   lambda)".chars(), Expr{car:None, cdr:None}, 0), false);
+    printexpr(parse(&mut "(\"q;abc\np\")".chars(), Expr{car:None, cdr:None}, 0, false, false, false), false);
 }
 
